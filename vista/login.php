72eb8d528c559a67d0518da7e6139d3f60cf4062
JohanPineda20 <johanmanuelph@ufps.edu.co>
2021-11-07 01:37:03 -0500

    segundo commit

--- vista/login.php
+++ vista/login.php
@@ -5,8 +5,43 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../css/styles.css">
+    <link rel="stylesheet" type="text/css" href="../css/css/all.min.css">
 </head>
 <body>
-    
+    <img class="wave" src="../img/wave.png" alt="">
+    <div class="contenedor">
+        <div class="img">
+            <img src="../img/imagen_login.png" alt="">
+        </div>
+        <div class="contenido-login">
+            <form action="" method="">
+                <img src="../img/icono_login.png" alt="">
+                <h2>Farmacia</h2>
+                <div class="input-div dni">
+                    <div class="i">
+                        <i class="fas fa-user"></i>
+                    </div>
+                    <div class="div">
+                        <h5>DNI</h5>
+                        <input type="text" name="user" class="input">
+                    </div>
+                </div>
+                <div class="input-div pass">
+                    <div class="i">
+                        <i class="fas fa-lock"></i>
+                    </div>
+                    <div class="div">
+                        <h5>Contraseña</h5>
+                        <input type="password" name="pass" class="input">
+                    </div>
+                </div>
+                <a href="">UFPS</a>
+                <input type="submit" class="btn" value="Iniciar sesión">
+            </form>
+        </div>
+    </div>
+<script src="../js/login.js"></script>
 </body>
 </html>
